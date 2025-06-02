36a11abd5ddf1e4527c6f69c32b37d819557dd76
etherealone0 <pranit.mails@gmail.com>
2025-06-02 16:51:14 +0530

    Built basic UI Layout

---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+
+import { 
+    SidebarGroup, 
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu, 
+    SidebarMenuButton, 
+    SidebarMenuItem
+} from "@/components/ui/sidebar";
+
+const items = [
+    {
+        title: "History",
+        url: "/playlist/history",
+        icon: HistoryIcon,
+    },
+    {
+        title: "Liked Videos",
+        url: "/playlists/like",
+        icon: ThumbsUpIcon,
+        auth: true,
+    },
+    {
+        title: "All Playlist",
+        url: "/playlists",
+        icon: ListVideoIcon,
+    }
+];
+
+export const PersonalSection = () => {
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent>
+                <SidebarGroupLabel>You</SidebarGroupLabel>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                asChild
+                                isActive={false}
+                                onClick={() => {}}
+                            >
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+}
