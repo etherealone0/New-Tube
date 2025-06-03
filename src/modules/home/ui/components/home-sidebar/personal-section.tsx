b9b263a3dec4a8f7f35ca0c4d00abdc9c8298327
etherealone0 <pranit.mails@gmail.com>
2025-06-03 06:46:55 +0530

    Introduced Authentication using clerk API

--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -2,6 +2,7 @@
 
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
 import Link from "next/link";
+import { useAuth, useClerk } from "@clerk/nextjs";
 
 import { 
     SidebarGroup, 
@@ -11,6 +12,7 @@ import {
     SidebarMenuButton, 
     SidebarMenuItem
 } from "@/components/ui/sidebar";
+import { auth } from "@clerk/nextjs/server";
 
 const items = [
     {
@@ -32,6 +34,9 @@ const items = [
 ];
 
 export const PersonalSection = () => {
+    const clerk = useClerk();
+    const { isSignedIn } = useAuth();
+
     return (
         <SidebarGroup>
             <SidebarGroupContent>
@@ -43,7 +48,12 @@ export const PersonalSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false}
-                                onClick={() => {}}
+                                onClick={(e) => {
+                                    if(!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    }
+                                }}
                             >
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon />
